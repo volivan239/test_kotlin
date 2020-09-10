1349c7a630caedbe7b84c33ffeb408347071cb69
Volkov Ivan <volkov.ivan2004@gmail.com>
2020-09-10 15:09:48 +0300

    added hw2 functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,10 @@ fun hw() {
     println("Hello, world!")
 }
 
+fun hw2() {
+    println("Hello, world! Hello, world!")
+}
+
 fun main() {
     val a = readLine()!!.split(" ").map { it.toInt() }.sorted()
     println(a)
